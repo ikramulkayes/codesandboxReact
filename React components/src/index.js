e492186a19d0dcca0d2d0a2ad1d8f92d7c1ede52
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-26 23:19:54 +0600

    Create index.js

---
+++ React components/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./heading";
+import List from "./list";
+//var k = heading()
+ReactDOM.render(
+  <div>
+    <Heading />
+    <List />
+  </div>,
+  document.getElementById("root")
+);
