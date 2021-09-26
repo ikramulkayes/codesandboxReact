31ba65d555bae99bdffa25f3bd1953bcc794960b
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-26 23:20:21 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-var food = "Teheri";
-var shop = "Salam";
-var reso = "Chicken";
-var resu = "Leg Piece";
-ReactDOM.render(
-  <div>
-    <h1 className="bb" contentEditable="true" spellCheck="false">
-      My Favourite Foods
-    </h1>
-    <ul>
-      <li>Dhaka's {shop + " " + food}</li>
-      <li>
-        {reso} {resu}
-      </li>
-      <li>Chiken Leg piece</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+import React from "react"
+import ReactDOM from "react-dom"
+var d = new Date(2021,1,1,13);
+var j = (d.getHours())
+//alert(j)
+if (j>=0 && j <= 12){
+  ReactDOM.render(<h1 className="heading" style={{color:"red"}}>Good Morning</h1>,document.getElementById("root"))
+}else if(j>12 && j<=18){
+  ReactDOM.render(<h1 className="heading" style={{color:"green"}}>Good Afternoon</h1>,document.getElementById("root"))
+}else{
+  ReactDOM.render(<h1 className="heading" style={{color:"blue"}}>Good Evening</h1>,document.getElementById("root"))
+}
