daeb68870516c18bd7dc9c890f2e9386a82763ed
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-28 02:02:45 +0600

    Create App.js

---
+++ keeper part 2/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+//import Note from "./Note";
+import First from "./first";
+function App() {
+  return (
+    <div>
+      <Header />
+      <First />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
