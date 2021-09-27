96906b3ab3dbd2bf86986d6b56cbe957e23e7234
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-28 02:02:30 +0600

    Create head.jsx

---
+++ keeper part 1/src/head.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import s from "./public/styles.css"
+function Header() {
+    return <header><h1>Keeper</h1></header>
+  }
+export default Header;
