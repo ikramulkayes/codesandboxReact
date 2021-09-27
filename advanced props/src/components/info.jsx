33cde5cbe1dc3db9d104e24b705319e83dee406d
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-28 02:02:01 +0600

    Create info.jsx

---
+++ advanced props/src/components/info.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Info(props){
+    return <div><p className = "info">{props.phone}</p>
+    <p className = "info">{props.email}</p>
+    </div>
+}
+export default Info;
